c619cd3c7657fde52f7e8de1f43b5cad2d012de8
Kingsman <Kingsman117@163.com>
2020-04-14 16:19:15 +0800

    Creat Zoom

---
+++ src/main/java/cn/edu/scujcc/ClickClickApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.scujcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickClickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickClickApplication.class, args);
+	}
+
+}
